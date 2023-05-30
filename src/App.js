4b48263f35e517d37b75964803a092675c366040
narendra-kp <124985031+narendra-kp@users.noreply.github.com>
2023-05-30 14:00:30 +0530

    Few changes to App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-        <h1> Welcome to Little Lemon Project Practice. </h1>
+          <h1> Welcome to Little Lemon Project Practice. </h1>
+          <h3>This is some sample test.</h3>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
